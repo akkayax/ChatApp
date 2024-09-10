4a5205619333b0fdd2a7ab710f8b9e92f45001ae
akkayax <zeynepsakkaya2@gmail.com>
2024-09-10 22:59:27 +0300

    Sending messages and storing messages in database made

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    user = models.CharField(max_length=255)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user}: {self.content[:50]}'
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import ChatRoom, Message
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -9,8 +11,19 @@ def chat_room(req):
     room_name = req.GET.get('room_name')
     user_name = req.GET.get('user_name')
 
+    room, created = ChatRoom.objects.get_or_create(name=room_name)
+
+    if req.method == 'POST':
+        message_content = req.POST.get('message')
+        if message_content:
+            Message.objects.create(room=room, user=user_name, content=message_content)
+
+    messages = Message.objects.filter(room=room).order_by('timestamp')
+
     context = {
         'room_name': room_name,
         'user_name': user_name,
+        'messages': messages,
     }
+    
     return render(req, 'chat_room.html', context)
