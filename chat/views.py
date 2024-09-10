54addf17c7d61d14ffc5d44997b37b4268905b0b
akkayax <zeynepsakkaya2@gmail.com>
2024-09-10 22:36:08 +0300

    basic unique chat room name and username functionality made

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(req):
+    return render(req, 'home.html')
+
+def chat_room(req):
+    room_name = req.GET.get('room_name')
+    user_name = req.GET.get('user_name')
+
+    context = {
+        'room_name': room_name,
+        'user_name': user_name,
+    }
+    return render(req, 'chat_room.html', context)
